7a378ed9ace4d936075f94ffea90aed210576ca0
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-23 12:26:27 -0400

    Add user id to exercises table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523014806) do
+ActiveRecord::Schema.define(version: 20140723151249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20140523014806) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "source_url"
+    t.integer  "user_id"
   end
+
+  add_index "exercises", ["user_id"], name: "index_exercises_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
