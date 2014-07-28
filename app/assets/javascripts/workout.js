f9be7fe10b763de5695c595132988fed683853d0
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-28 16:49:42 -0400

    Add inline form for creating a new workout

---
+++ app/assets/javascripts/workout.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('#plus-workout').click(App.showWorkoutForm);
+  $('#cncl-workout').click(App.hideWorkoutForm);
+});
+
+var App = App || {};
+
+App.showWorkoutForm = function() {
+    $('#workout-form').slideToggle();
+    $('#plus-workout').hide();
+  };
+
+App.hideWorkoutForm = function() {
+  $('#workout-form').slideToggle();
+  $('#plus-workout').show();
+  };
