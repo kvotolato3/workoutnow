1c8b737de1ad279e13f3e5d9e851e44e4a81198a
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-29 16:19:23 -0400

    Add javascript to hide/show new workout form
    - ajax update is still in progress

--- app/assets/javascripts/workout.js
+++ app/assets/javascripts/workout.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
   $('#plus-workout').click(App.showWorkoutForm);
   $('#cncl-workout').click(App.hideWorkoutForm);
+  $('#inline-workout-form').submit(App.saveWorkout);
 });
 
 var App = App || {};
 
 App.showWorkoutForm = function() {
-    $('#workout-form').slideToggle();
-    $('#plus-workout').hide();
-  };
+  $('#workout-form').slideToggle();
+  $('#plus-workout').hide();
+};
 
 App.hideWorkoutForm = function() {
   $('#workout-form').slideToggle();
   $('#plus-workout').show();
-  };
+};
+
+App.saveWorkout = function() {
+  event.preventDefault();
+  var $name = $('input[name=workout_name]').val();
+  $.ajax({
+    url: '/api/v1/workouts',
+    type: 'POST',
+    data: {'workout': {'name': $name}},
+    dataType: 'json'
+  }).done(App.appendMyWorkouts);
+};
+
+App.appendMyWorkouts = function(result) {
+  console.log(result);
+  var newElement = $('<tr><td></td><td></td></tr>');
+  console.log(newElement);
+
+};
