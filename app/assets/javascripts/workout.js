fbe70b3a761207831c1eeb1cead71c1dde21faba
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-30 11:54:18 -0400

    Add event listeners for draggable elements

--- app/assets/javascripts/workout.js
+++ app/assets/javascripts/workout.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
   $('#plus-workout').click(App.showWorkoutForm);
   $('#cncl-workout').click(App.hideWorkoutForm);
+  $('.exercise-container').on('dragstart', function(e, ui){
+    var exId = $(this).attr('data-exercise-id');
+    e.data = {'exerciseId': exId};
+    e.originalEvent.dataTransfer.effectAllowed = "copy";
+    console.log("data: " + e.data);
+  });
+  $('.wo-row').on('dragover', function(e){
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.originalEvent.dataTransfer.dropEffect = "copy";
+    return false;
+  });
 });
 
 var App = App || {};
