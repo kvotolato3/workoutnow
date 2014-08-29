9e328e3f0fb3dc597aa355aa2f7bcab2ad048d8e
Kathleen Votolato <kvotolato3@gmail.com>
2014-08-29 13:57:56 -0400

    Add route for workout_exercises api

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :workouts
+      resources :workout_exercises
     end
   end
 
